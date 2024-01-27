76a88f09dcc6b0e47bce3340ad448375a6287461
satvikkalyan <satvikkalyan@gmail.com>
2024-01-26 20:42:50 -0500

    Added .env File

--- src/components/utility/constants.js
+++ src/components/utility/constants.js
@@ -1,7 +1,7 @@
 import placeholderpic from "./../../resources/images/userprofile.jpeg"
 
 const API = `http://localhost:8081`
-export const BASE_URL = `http://localhost:8080`
+export const BASE_URL = process.env.REACT_APP_BASE_URL
 export const userAPI = `${API}/api/v1/users/`;
 export const roomsCountAPI = `${API}/api/v1/admins/roomscount/`;
 export const usersCountAPI =
